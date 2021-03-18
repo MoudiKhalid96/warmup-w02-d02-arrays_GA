3b71bd4106dc3c2c889b06133372d0b7b223d4bc
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-03-18 04:09:21 +0300

    warmup completed

--- solution.js
+++ solution.js
@@ -1,37 +1,31 @@
-/*
-Problem
-Create a function that takes an array of integers as an argument and returns an array, where the first element is the count of positive numbers and the second element is sum of negative numbers.
 
-Example
-Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15].
-
-Output: [10, -65].
-
-Bonus
-If the input array is empty or null, return an empty array.
-*/
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
-
-function countPositiveAndSumNegative(array){
+function countPositiveAndSumNegative(array) {
+  var result = []
   let countPositive = 0;
   let sumOfNegative = 0;
-  
-  if(array.length === 0){
-    console.log("This array is empty");
-    return;
-  }
-  else if(array.length !== 0){
-    for(let i=0 ; i<array.length ; i++){
-      if(array[i] > 0){
+
+  if (array.length === 0)
+    return "It's empty !";
+
+  else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > 0) {
         countPositive++;
-        
-      }else if(array[i] <= -1){
-        sumOfNegative += array[i]; 
+
+      } else if (array[i] <= -1) {
+        sumOfNegative += array[i];
       }
-      
-    }//end for loop.
-    
+
+    }
+
   }
-  console.log("Number of positive numbers: " + countPositive);
-  console.log("Summation of negative numbers: " + sumOfNegative);
-}//end function.
+
+  result[0] = countPositive;
+  result[1] = sumOfNegative;
+
+  return result;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+
+console.log(countPositiveAndSumNegative(numbers))
